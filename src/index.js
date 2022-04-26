e0f96585ce3edc3a97c1d3ad5ff305bbdd9499c0
txqcoder <17693430686@163.com>
2022-04-26 18:14:39 +0800

    '初始化目录'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +13,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 浏览器数据统计。项目运行日志上报，此处也可以填写后端服务。
+// reportWebVitals(console.log);
