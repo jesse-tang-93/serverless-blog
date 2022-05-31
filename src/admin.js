aab2cf57d73478cb339a9dc7b66d7e228ca8d1b3
txqcoder <17693430686@163.com>
2022-05-31 15:49:26 +0800

    style: 修改单页入口为多页入口。添加访问admin的支持

---
+++ src/admin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>admin page</div>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 浏览器数据统计。项目运行日志上报，此处也可以填写后端服务。
+// reportWebVitals(console.log);
